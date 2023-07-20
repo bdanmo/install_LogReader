0a031f3b9c85353a965e5245fd0634e21b6f9e40
Brendan Moran <b.daniel.moran@gmail.com>
2023-07-20 12:43:02 -0400

    general error handling

--- remediation.ps1
+++ remediation.ps1
@@ -1,11 +1,15 @@
-#copy LogReader.exe from CWD to C:\Users\Public\Desktop
-Copy-Item -Path .\LogReader.exe -Destination "C:\Users\Public\Desktop\LogReader.exe"
-#check if LogReader.exe is in C:\Users\Public\Desktop
-$LogReader = Test-Path -Path "C:\Users\Public\Desktop\LogReader.exe"
-if ($LogReader -eq $false) {
-    Write-Host "LogReader.exe is not in C:\Users\Public\Desktop"
-    exit 1
-} elseif ($LogReader -eq $true) {
-    Write-Host "LogReader.exe is in C:\Users\Public\Desktop"
-    exit 0
+try {
+    #copy LogReader.exe from CWD to C:\Users\Public\Desktop
+    Copy-Item -Path .\LogReader.exe -Destination "C:\Users\Public\Desktop\LogReader.exe"
+    #check if LogReader.exe is in C:\Users\Public\Desktop
+    $LogReader = Test-Path -Path "C:\Users\Public\Desktop\LogReader.exe"
+    if ($LogReader -eq $false) {
+        Write-Host "LogReader.exe is not in C:\Users\Public\Desktop"
+        exit 1
+    } elseif ($LogReader -eq $true) {
+        Write-Host "LogReader.exe is in C:\Users\Public\Desktop"
+        exit 0
+    }
+} catch {
+    Write-Output $_.Exception.Message
 }
